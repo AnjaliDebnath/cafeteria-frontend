822229fdec38ca7ed5cd3dc353d69374bc4694f0
AnjaliDebnath <anjalidebnath1003@gmail.com>
2025-01-28 15:10:26 +0530

    initiated a frontend for my cafeteria website

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Counter from "./pages/Counter"
+import Cart from "./pages/Cart";
+import Merchant from "./pages/Merchant";
+import Dishes from "./pages/Dishes";
+import Navbar from './components/Navbar';
+
+
+import './App.css'
+import CounterDishes from './pages/CounterDishes';
+import Register from './pages/Register';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  
+
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element= {<Home/>}/>
+        <Route path='/counter' element= {<Counter/>}/>
+        <Route path='/counterDishes' element= {<CounterDishes/>}/>
+        <Route path='/cart' element= {<Cart/>}/>
+        <Route path='/allDishes' element= {<Dishes/>}/>
+        <Route path='/merchant' element= {<Merchant/>}/>
+      
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        </Routes>
+    </Router>
+  )
+}
+
+export default App
